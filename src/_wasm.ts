0df16a324fea1b41e8d28641610d1dfae1cb1907
Nicholas Berlette <nick@berlette.com>
2025-11-25 04:29:50 -0800

    refactor: comment out unused adapters code for now

--- src/_wasm.ts
+++ src/_wasm.ts
@@ -2,23 +2,23 @@
 // deno-coverage-ignore-start
 import * as wasm from "../lib/comrak_wasm.js";
 
-const setPrototypeOf = Object.setPrototypeOf;
-const isPrototypeOf = Object.isPrototypeOf;
-// Set up prototype chain for the syntect adapter class
-// (janky, but otherwise the wasm-bindgen glue will throw errors)
-if (
-  "SyntectAdapter" in wasm &&
-  typeof wasm.SyntectAdapter === "function" &&
-  "SyntaxHighlighterAdapter" in wasm &&
-  typeof wasm.SyntaxHighlighterAdapter === "function"
-) {
-  const target = wasm.SyntectAdapter.prototype;
-  const proto = wasm.SyntaxHighlighterAdapter.prototype;
+// const setPrototypeOf = Object.setPrototypeOf;
+// const isPrototypeOf = Object.isPrototypeOf;
+// // Set up prototype chain for the syntect adapter class
+// // (janky, but otherwise the wasm-bindgen g lue will throw errors)
+// if (
+//   "SyntectAdapter" in wasm &&
+//   typeof wasm.SyntectAdapter === "function" &&
+//   "SyntaxHighlighterAdapter" in wasm &&
+//   typeof wasm.SyntaxHighlighterAdapter === "function"
+// ) {
+//   const target = wasm.SyntectAdapter.prototype;
+//   const proto = wasm.SyntaxHighlighterAdapter.prototype;
 
-  if (target && proto && !isPrototypeOf.call(proto, target)) {
-    setPrototypeOf(target, proto);
-  }
-}
+//   if (target && proto && !isPrototypeOf.call(proto, target)) {
+//     setPrototypeOf(target, proto);
+//   }
+// }
 
 export default wasm;
 
